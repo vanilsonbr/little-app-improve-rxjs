164e23529c36a265b2163708b1b9202623c004cf
Vanilson Nogueira <vanilson.nogueira@gmail.com>
2020-08-08 23:18:40 -0300

    using banckend api

--- src/app/students.service.ts
+++ src/app/students.service.ts
@@ -3,30 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
 
 export interface Student {
-  name: string;
-  id: number;
+  UserName: string;
+  ID: number;
 }
 
 @Injectable()
 export class StudentsService {
-
-  private students: Student[] = [
-    { name: "van", id: 1 },
-    { name: "joy", id: 2 },
-    { name: "foo", id: 3 },
-    { name: "bar", id: 4 },
-    { name: "gen", id: 5 },
-    { name: "jin", id: 6 },
-    { name: "ken", id: 7 },
-    { name: "gal", id: 8 },
-    { name: "kim", id: 9 },
-    { name: "han", id: 10 },
-    { name: "kal", id: 11 },
-  ];
-
   constructor(private http: HttpClient) { }
 
   getStudents = () =>
-     of(this.students);
-    // this.http.get<Student>('api/Students');
+    this.http.get<Student[]>('http://fakerestapi.azurewebsites.net/api/Users');
 }
--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,39 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentsService, Student } from '../students.service';
-import { switchMap, catchError, map, tap,  } from 'rxjs/operators';
-import { Subject, of, Subscription, Observable } from 'rxjs';
+import { switchMap, catchError, tap,  } from 'rxjs/operators';
+import { Subject, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.scss']
+  styleUrls: ['./students.component.scss'],
+  providers: [StudentsService]
 })
 export class StudentsComponent implements OnInit {
   studentsSearchInput$ = new Subject<void>();
-  subscriptions: Subscription[];
-  students: { name: string; id: number; selected: boolean; }[];
-  loading: boolean = false;
+  subscriptions$: Subscription[];
+  students: Student[];
+  loading: boolean;
 
   constructor(private studentsService: StudentsService) { }
 
   ngOnInit(): void {
     let sub$ = this.studentsSearchInput$.pipe(
       tap(() => {
-        this.students = [];
+        this.students = null;
         this.loading = true;
       }),
-      switchMap(_ => this.studentsService.getStudents().pipe(catchError(e => of([] as Student[])))),
-      map(students => students.map(stds => ({ name: stds.name, id: stds.id, selected: false }))),
-      tap(() => this.loading = false)
-    ).subscribe(studentsPlusSelected => {
-      this.students = studentsPlusSelected;
+      switchMap(_ => this.studentsService.getStudents().pipe(
+        catchError(error => {
+          alert(error);
+          return of([] as Student[]);
+        })
+      )),
+      tap(() => {
+        this.loading = false;
+      })
+    ).subscribe(students => {
+      this.students = students;
     });
 
-    this.subscriptions = [sub$];
+    this.subscriptions$ = [sub$];
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach(sub => {
+    this.subscriptions$.forEach(sub => {
       if(!sub.closed)
         sub.unsubscribe();
     })
